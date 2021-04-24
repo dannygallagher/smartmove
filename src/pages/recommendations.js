c1fa953b5b6d1a6e595e51ad0658a4f52bd3b938
Steven Wu <swu0430@gmail.com>
2021-04-24 13:38:13 -0400

    Router updates

---
+++ src/pages/recommendations.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function Recommendations() {
+    return (
+        <>
+            <div>
+                Map Goes Here
+            </div>
+        </>
+    )
+}
