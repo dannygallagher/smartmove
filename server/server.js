71ae42b36f1d5574e618ea89678f5e23f1b3b86c
Dan Gallagher <dgallagher757@gmail.com>
2021-05-09 13:15:14 -0400

    Updated temporary tables to make multiple smaller tables instead of one big one. Helps with query modularity, performance, and accuracy

--- server/server.js
+++ server/server.js
@@ -14,66 +14,92 @@ const config = {
     password: "group58password",
     database: "cis550_proj_db",
     port: "3306"
+
+}
+
+const configMult = {
+    host: "cis550-proj.c4nnu48yetwb.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "group58password",
+    database: "cis550_proj_db",
+    port: "3306",
+    multipleStatements: true
+
 }
 
 config.connectionLimit = 10;
-const connection = mysql.createPool(config)
+const connection = mysql.createPool(configMult)
 
 // Temporary table query
 app.post('/temptablecreate', (req, res) => {
     var query = `
-        CREATE TABLE att_rats
-        WITH gfk_count AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS att
-            FROM business
-            WHERE attributesGoodForKids = 'True'
-            GROUP BY postal_code),
-        gfk_total AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS total
-            FROM business
-            WHERE attributesGoodForKids IN ('Unlisted', 'True', 'False')
-            GROUP BY postal_code),
-        gfd_count AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS att
-            FROM business
-            WHERE attributesGoodForDancing = 'True'
-            GROUP BY postal_code),
-        gfd_total AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS total
-            FROM business
-            WHERE attributesGoodForDancing IN ('Unlisted', 'True', 'False')
-            GROUP BY postal_code),
-        wa_count AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS att
-            FROM business
-            WHERE attributesWheelchairAccessible = 'True'
-            GROUP BY postal_code),
-        wa_total AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS total
-            FROM business
-            WHERE attributesWheelchairAccessible IN ('Unlisted', 'True', 'False')
-            GROUP BY postal_code),
-        da_count AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS att
-            FROM business
-            WHERE attributesDogsAllowed = 'True'
-            GROUP BY postal_code),
-        da_total AS(
-            SELECT postal_code AS zipcode, COUNT(business_id) AS total
-            FROM business
-            WHERE attributesDogsAllowed IN ('Unlisted', 'True', 'False')
-            GROUP BY postal_code),
-        att_ratio AS(
-            SELECT gfk_count.zipcode, gfk_count.att AS GoodForKidsCount, gfk_total.total AS GoodForKidsTotal, gfk_count.att/gfk_total.total AS GoodForKidsRatio, ROUND(PERCENT_RANK() OVER (ORDER BY gfk_count.att/gfk_total.total), 2) AS GoodForKidsPercentile,
-            gfd_count.att AS GoodForDancingCount, gfd_total.total AS GoodForDancingTotal, gfd_count.att/gfd_total.total AS GoodForDancingRatio, ROUND(PERCENT_RANK() OVER (ORDER BY gfd_count.att/gfd_total.total), 2) AS GoodForDancingPercentile,
-            wa_count.att AS WheelchairAccessibleCount, wa_total.total AS WheelchairAccessibleTotal, wa_count.att/wa_total.total AS WheelchairAccessibleRatio, ROUND(PERCENT_RANK() OVER (ORDER BY wa_count.att/wa_total.total), 2) AS WheelchairAccessiblePercentile,
-            da_count.att AS DogsAllowedCount, da_total.total AS DogsAllowedTotal, da_count.att/da_total.total AS DogsAllowedRatio, ROUND(PERCENT_RANK() OVER (ORDER BY da_count.att/da_total.total), 2) AS DogsAllowedPercentile
-            FROM gfk_count JOIN gfk_total ON gfk_count.zipcode = gfk_total.zipcode
-            JOIN gfd_count ON gfk_count.zipcode = gfd_count.zipcode JOIN gfd_total ON gfk_count.zipcode = gfd_total.zipcode
-            JOIN wa_count ON gfk_count.zipcode = wa_count.zipcode JOIN wa_total ON gfk_count.zipcode = wa_total.zipcode
-            JOIN da_count ON gfk_count.zipcode = da_count.zipcode JOIN da_total ON gfk_count.zipcode = da_total.zipcode)
-        SELECT *
-        FROM att_ratio;
+        CREATE TABLE GoodForKids
+WITH att_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS att
+    FROM business
+    WHERE attributesGoodForKids = 'True'
+    GROUP BY postal_code),
+total_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS total
+    FROM business
+	WHERE attributesGoodForKids IN ('Unlisted', 'True', 'False')
+    GROUP BY postal_code),
+att_ratio AS(
+	SELECT a.zipcode, a.att, t.total, a.att/t.total AS att_rat, ROUND(PERCENT_RANK() OVER (ORDER BY a.att/t.total), 2) AS percentile
+	FROM att_count a JOIN total_count t ON a.zipcode = t.zipcode)
+SELECT *
+FROM att_ratio;
+
+CREATE TABLE GoodForDancing
+WITH att_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS att
+    FROM business
+    WHERE attributesGoodForDancing = 'True'
+    GROUP BY postal_code),
+total_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS total
+    FROM business
+	WHERE attributesGoodForDancing IN ('Unlisted', 'True', 'False')
+    GROUP BY postal_code),
+att_ratio AS(
+	SELECT a.zipcode, a.att, t.total, a.att/t.total AS att_rat, ROUND(PERCENT_RANK() OVER (ORDER BY a.att/t.total), 2) AS percentile
+	FROM att_count a JOIN total_count t ON a.zipcode = t.zipcode)
+SELECT *
+FROM att_ratio;
+
+CREATE TABLE WheelchairAccessible
+WITH att_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS att
+    FROM business
+    WHERE attributesWheelchairAccessible = 'True'
+    GROUP BY postal_code),
+total_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS total
+    FROM business
+	WHERE attributesWheelchairAccessible IN ('Unlisted', 'True', 'False')
+    GROUP BY postal_code),
+att_ratio AS(
+	SELECT a.zipcode, a.att, t.total, a.att/t.total AS att_rat, ROUND(PERCENT_RANK() OVER (ORDER BY a.att/t.total), 2) AS percentile
+	FROM att_count a JOIN total_count t ON a.zipcode = t.zipcode)
+SELECT *
+FROM att_ratio;
+
+CREATE TABLE DogsAllowed
+WITH att_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS att
+    FROM business
+    WHERE attributesDogsAllowed = 'True'
+    GROUP BY postal_code),
+total_count AS(
+	SELECT postal_code AS zipcode, COUNT(business_id) AS total
+    FROM business
+	WHERE attributesDogsAllowed IN ('Unlisted', 'True', 'False')
+    GROUP BY postal_code),
+att_ratio AS(
+	SELECT a.zipcode, a.att, t.total, a.att/t.total AS att_rat, ROUND(PERCENT_RANK() OVER (ORDER BY a.att/t.total), 2) AS percentile
+	FROM att_count a JOIN total_count t ON a.zipcode = t.zipcode)
+SELECT *
+FROM att_ratio;
     `;
 
     connection.query(query, function(err, rows, fields) {
@@ -89,7 +115,10 @@ app.post('/temptablecreate', (req, res) => {
 // Temporary table query
 app.post('/temptabledrop', (req, res) => {
     var query = `
-        DROP TABLE att_rats
+        DROP TABLE GoodForKids;
+        DROP TABLE GoodForDancing;
+        DROP TABLE WheelchairAccessible;
+        DROP TABLE DogsAllowed;
     `;
 
     connection.query(query, function(err, rows, fields) {
